fa92a0da56996a92d801b3e4172782e88730fb88
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-08-13 14:06:09 +0300

    Update Dictionaries.swift

--- Dictionaries.swift
+++ Dictionaries.swift
@@ -1,4 +1,15 @@
-//Morse Decorder
+/*
+Morse Decoder by Bakhrom Usmanov
+Morse code, named after American artist Samuel Finley Breese Morse, is a method of telecommunication that visually encodes letters into unique combinations of dots . and dashes -:
+
+Morse code has been used as a communication aide in aviation, radio, assistive technology, and more.
+
+Here are a few rules to know when deciphering a Morse code message:
+
+Each character is separated by a single space.
+Each word is separated by three spaces.
+Capitalization does not matter.
+/*
 
 var englishText = "this is a secret message"
 
